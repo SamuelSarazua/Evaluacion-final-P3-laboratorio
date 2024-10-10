b2f35ad1132cb73b4fcb6a2a18c594d8928e4c39
Samue Sarazua <sesarazua@scl.edu.gt>
2024-10-10 11:43:05 -0600

    Se termina el componente 2

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { cuadrito1 } from "./componentes/componente1/componente1.js";
+import { cuadrito2 } from "./componentes/componente2/componente2.js";
 
 let DOM = document.querySelector("#root");
 
 let proyect = document.createElement(`section`);
 proyect.className = "proyect";
 proyect.appendChild(cuadrito1);
+proyect.appendChild(cuadrito2);
 DOM.appendChild(proyect);
