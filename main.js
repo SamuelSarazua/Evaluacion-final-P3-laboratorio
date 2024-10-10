730183daac552fc9eba079c577ffa9d66df0b9d8
Samue Sarazua <sesarazua@scl.edu.gt>
2024-10-10 11:57:59 -0600

    Se termina el componente 3

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { cuadrito1 } from "./componentes/componente1/componente1.js";
 import { cuadrito2 } from "./componentes/componente2/componente2.js";
+import { cuadrito3 } from "./componentes/componente3/componente3.js";
 
 let DOM = document.querySelector("#root");
 
@@ -7,4 +8,5 @@ let proyect = document.createElement(`section`);
 proyect.className = "proyect";
 proyect.appendChild(cuadrito1);
 proyect.appendChild(cuadrito2);
+proyect.appendChild(cuadrito3)
 DOM.appendChild(proyect);
